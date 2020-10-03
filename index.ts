7ff90369f6d4c5fede70e8602dfdcc4250a66520
nhcarrigan <nhcarrigan@gmail.com>
2020-10-03 11:58:43 -0700

    Remove ping

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ client
 
 client.on("message", (message: Message) => {
     if (message.content.toLowerCase().includes("matt") || message.author.id === "541305895544422430") {
-        message.channel.send("We love <@!541305895544422430>!")
+        message.channel.send("We love Matt")
         message.react("💜")
     }
 });
