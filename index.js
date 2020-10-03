fd5e7086e09bbe70303342fb7b15eece0ffe4e10
nhcarrigan <nhcarrigan@gmail.com>
2020-10-03 11:37:35 -0700

    Initial bot setup

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var discord_js_1 = require("discord.js");
+var dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+var client = new discord_js_1.Client();
+var token = process.env.TOKEN;
+client
+    .login(token)
+    .then(function () { return console.log("It's alive!"); })
+    .catch(function (err) { return console.error(err); });
+client.on("message", function (message) {
+    if (message.content.toLowerCase().includes("matt") || message.author.id === "541305895544422430") {
+        message.channel.send("We love <@!541305895544422430>!");
+        message.react("💜");
+    }
+});
---
+++ index.ts
@@ -0,0 +1,18 @@
+import { Client, Message } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const client = new Client();
+const token = process.env.TOKEN;
+
+client
+  .login(token)
+  .then(() => console.log("It's alive!"))
+  .catch((err) => console.error(err));
+
+client.on("message", (message: Message) => {
+    if (message.content.toLowerCase().includes("matt") || message.author.id === "541305895544422430") {
+        message.channel.send("We love <@!541305895544422430>!")
+        message.react("💜")
+    }
+});
