ed171aab88508a61cfeb4d0dab3aca559066e09f
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-03-18 20:52:33 -0700

    feat: clean up login logic
    Clean up the login logic, adding a ready event listener to verify
    bot is online.

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,13 @@ import { onMessage } from "./events/onMessage";
 const client = new Client();
 const token = process.env.TOKEN;
 
-//connect the bot
-client
-  .login(token)
-  .then(() => console.log("It's alive!"))
-  .catch((err) => console.error(err));
+if (!token) {
+  console.error("Missing Discord Token");
+  process.exit(1);
+}
 
 client.on("message", (message) => onMessage(message, client));
+
+client.on("ready", () => console.log("bot ready!"));
+
+client.login(token);
