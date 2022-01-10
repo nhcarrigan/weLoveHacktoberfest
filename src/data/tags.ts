fd66159738b34f32de493f488e162de4bf7f9aec
Olivia (Zoe) <zoe.i2k1@gmail.com>
2022-01-10 15:09:21 +0100

    fix: swap 4 and 5

--- src/data/tags.ts
+++ src/data/tags.ts
@@ -106,7 +106,7 @@ export const tags: Tag[] = [
   {
     name: "shipfaster",
     content:
-      "How to get your Hacktoberfest Swag faster:\n1. Sacrifice a goat\n2. Realize what you've just done for a t-shirt, and go for a walk\n3. Wait a month, and perform the [Ritual of Cleansing](https://www.youtube.com/watch?v=zL19uMsnpSU)\n4. Forget about the shirt\n5. Realize there's more to life than waiting for a free t-shirt that may or may not arrive arrive soon\n6. Be surprised when your swag finally appears after you've completely forgotten the entire thing\n7. Thank <@541305895544422430> for being so awesome",
+      "How to get your Hacktoberfest Swag faster:\n1. Sacrifice a goat\n2. Realize what you've just done for a t-shirt, and go for a walk\n3. Wait a month, and perform the [Ritual of Cleansing](https://www.youtube.com/watch?v=zL19uMsnpSU)\n4. Realize there's more to life than waiting for a free t-shirt that may or may not arrive arrive soon\n5. Forget about the shirt\n6. Be surprised when your swag finally appears after you've completely forgotten the entire thing\n7. Thank <@541305895544422430> for being so awesome",
     title: "How do I get my swag faster?",
     aliases: [],
   },
