2972787de73d567c70653da2d969f16ddca83ff1
Naomi Carrigan <nhcarrigan@gmail.com>
2023-10-13 09:28:59 -0700

    feat(tools): infrastructure for full regression testing (#785)

---
+++ test/__statics__/RegexRegressions.ts
@@ -0,0 +1,9 @@
+const Regression2023Oct13 = `Hi, @everyone a simple backend project is here: 
+The Moments
+It is a website made for ANIME lovers, where they can post/write their best moments in anime and can comment on each other's post
+Stacks used: Nodejs, Express, MongoDB and Bootstrap 
+GitHub link https://github.com/gauravtomar7/The-Moments
+
+This needs just a Frontend touch, so you can contribute to it by improving its frontend by using simple CSS.`;
+
+export const RegressionsFromProduction = [Regression2023Oct13];
